793e410a5d3bea4f72ff4f5c895138b185ae5770
Evgeniy <e.koskov@bk.ru>
2024-12-10 23:18:34 +0500

    add structures

--- main.cpp
+++ main.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+#pragma pack(push, 1)
+
+struct VolumeHeaderFromat
+{
+    uint16_t header_crc;
+    uint8_t header_type;
+    uint16_t header_flags;
+    uint16_t header_size;
+
+};
+
+struct FileHead
+{
+    uint32_t PackSize;
+    uint32_t UnpSize;
+    uint8_t HostOS;
+    uint32_t FileCRC;
+    uint32_t FileTime;
+    uint8_t UnpVer;
+    uint8_t Method;
+    uint16_t NameSize;
+    uint32_t FileAttr;
+};
+
+#pragma pack(pop)
+
+
 int main()
 {
     cout << "Hello world!" << endl;
