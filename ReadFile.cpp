ee8732453582fb14820fafe131bf02569a870c60
Evgeniy <e.koskov@bk.ru>
2024-12-11 16:19:59 +0500

    final

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -7,13 +7,39 @@
 
 using namespace std;
 
+#pragma pack(push, 1)
+
+struct VolumeHeaderFormat
+{
+    uint16_t header_crc;
+    uint8_t header_type;
+    uint16_t header_flags;
+    uint16_t header_size;
+
+};
+
+struct FileHead
+{
+    uint32_t PackSize;
+    uint32_t UnpSize;
+    uint8_t HostOS;
+    uint32_t FileCRC;
+    uint32_t FileTime;
+    uint8_t UnpVer;
+    uint8_t Method;
+    uint16_t NameSize;
+    uint32_t FileAttr;
+};
+
+#pragma pack(pop)
+
 void FileRead(const string& filename)
 {
     ifstream rar_file(filename, ios::binary);
 
     if (rar_file.is_open())
     {
-        cout << "File has been opened" << endl;
+        cout << "Файл был открыт." << endl;
     }
 
     rar_file.seekg(0, ios::end);
@@ -25,4 +51,46 @@ void FileRead(const string& filename)
     rar_file.read(reinterpret_cast<char*>(rar_data.data()), fileSize);
 
     rar_file.close();
+
+    cout << "Оглавления архива:" << endl;
+
+    size_t offset = 7;
+
+    int count = 1;
+
+    while (offset < rar_data.size())
+    {
+        const VolumeHeaderFormat* volume_header = reinterpret_cast<const VolumeHeaderFormat*>(&rar_data[offset]);
+
+        if (volume_header->header_type == 0x74)
+        {
+
+            if (offset + sizeof(FileHead) + sizeof(VolumeHeaderFormat) > rar_data.size())
+            {
+                break;
+            }
+
+            const FileHead* file_head = reinterpret_cast<const FileHead*>(&rar_data[offset + sizeof(VolumeHeaderFormat)]);
+
+            size_t offset_n = offset + sizeof(VolumeHeaderFormat) + sizeof(FileHead);
+
+            if (offset_n + file_head->NameSize > rar_data.size())
+            {
+                break;
+            }
+
+            string file_name(reinterpret_cast<const char*>(&rar_data[offset_n]), file_head->NameSize);
+
+            cout << count << ". " << file_name << endl;
+
+            count ++;
+
+            offset += volume_header->header_size + file_head->PackSize;
+        }
+
+        else
+        {
+            offset += volume_header->header_size;
+        }
+    }
 }
